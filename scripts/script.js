b3b60fa0f7f91d4e5ed36e9e4443c78ec8e5cacc
Jonathan <jonathan.gaul1@gmail.com>
2023-07-06 14:38:57 -0400

    Created playRound function
    The play round function plays one round of rock, paper scissors
    and prints to the console the winner of the round

--- scripts/script.js
+++ scripts/script.js
@@ -11,8 +11,47 @@ function getComputerChoice() {
     }
 }
 
+function getPlayerChoice() {
+    return prompt("Rock, paper or scissors?").trim().toLowerCase();
+}
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return `Tie! You picked ${playerChoice} and the computer picked ${computerChoice}.`;
+    }
+    let playerWon;
+    switch (playerChoice) {
+        case "rock":
+            computerChoice === "scissors" ? playerWon = true : playerWon = false;
+            if (playerWon) {
+                return `You chose ${playerChoice} and the computer chose ${computerChoice}. You won!`;
+            } else {
+                return `You chose ${playerChoice} and the computer chose ${computerChoice}. You lose!`;
+            }
+        case "paper":
+            computerChoice === "rock" ? playerWon = true : playerWon = false;
+            if (playerWon) {
+                return `You chose ${playerChoice} and the computer chose ${computerChoice}. You won!`;
+            } else {
+                return `You chose ${playerChoice} and the computer chose ${computerChoice}. You lose!`;
+            }
+        case "scissors":
+            computerChoice === "paper" ? playerWon = true : playerWon = false;
+            if (playerWon) {
+                return `You chose ${playerChoice} and the computer chose ${computerChoice}. You won!`;
+            } else {
+                return `You chose ${playerChoice} and the computer chose ${computerChoice}. You lose!`;
+            }
+        
+        default:
+            return 'You need to pick rock, paper or scissors!';
+    }
+}
+
 function playGame() {
-    console.log(getComputerChoice());
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+    console.log(playRound(playerChoice, computerChoice));
 }
 
 
